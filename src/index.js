34681abec6fd15a6364d19b7e61941753ba2b7dc
Carlos <cpacheco4983@gmail.com>
2018-08-21 03:13:20 -0400

    remove extra files. draw X on click

--- src/index.js
+++ src/index.js
@@ -1,8 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Square extends React.Component {
+  constructor(props) {
+    //all components that have a constructor should call super(props)
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
+
+  render() {
+    return (
+      <button className="square"
+      //not adding '() =>' causes alert to happen on Component render instad of onClick
+      //Is is something related to passing by value and passing by reference?
+      //https://www.reddit.com/r/reactjs/comments/93mk1j/beginners_thread_easy_question_august_2018/e4jt9cp/
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
